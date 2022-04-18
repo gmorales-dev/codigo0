8c1054f5ef24212a4936def6983ba1e4734c5172
gmorales <gmorales.lp1993@gmail.com>
2022-04-18 15:25:55 -0400

    probando etiqueta p

--- _js/main.js
+++
@@ -1,7 +0,0 @@
-// let parrafo = document.getElementsByClassName("p");
-
-// parrafo.addEventListener("click",()=>{
-//     console.log("este es una etiqueta p");
-// })
-
-console.log("aqui js");
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-// let parrafo = document.getElementsByClassName("p");
+let parrafo = document.getElementsByClassName("p");
 
-// parrafo.addEventListener("click",()=>{
-//     console.log("este es una etiqueta p");
-// })
+parrafo.addEventListener("click",()=>{
+    console.log("este es una etiqueta p");
+})
 
 console.log("aqui js");
